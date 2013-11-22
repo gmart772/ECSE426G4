9dae1b489a038190bd797b89f3b582b7e8c26098
unknown <jeansebastien.dery@gmail.com>
2013-11-22 14:15:28 -0500

    The LCD display works

--- Group4_RoboticHand_Tests/lcdManager.c
+++ Group4_RoboticHand_Tests/lcdManager.c
@@ -35,16 +35,30 @@ void setEnable(gpioState state);
  * Initializes the LCD display.
 */
 void initializeLCD(void) {
-	printf("[INFO] Initializing the LCD.");	
+	printf("[INFO] Initializing the LCD.\n");	
 	
 	initializeControlGPIO();
 	initializeDataGPIO();
 	
 	// Sets the Read/Write pin to 0 since we only write to the LCD.
 	setReadWrite(OFF);
+	// The default value for enable is 0 if there is no incoming data.
+	setEnable(OFF);
 	
 	setCommandOnDataLine(CLEAR_DISPLAY);
 	sendCommand();
+	
+	setCommandOnDataLine(RESET_CURSOR);
+	sendCommand();
+	
+	setCommandOnDataLine(INCREMENT_CHARACTER_MODE);
+	sendCommand();
+	
+	setCommandOnDataLine(DISPLAY_CURSOR_OFF);
+	sendCommand();
+	
+	char* testString = "This is a test.";
+	writeString(testString);
 }
 
 /**
@@ -273,10 +287,44 @@ void setCommandOnDataLine(lcdCommands commandToExecute) {
 			setData7(OFF);
 		break;
 		case RESET_CURSOR:
-			
+			setData0(OFF);
+			setData1(ON);
+			setData2(OFF);
+			setData3(OFF);
+			setData4(OFF);
+			setData5(OFF);
+			setData6(OFF);
+			setData7(OFF);
 		break;
 		case INCREMENT_CHARACTER_MODE:
-			
+			setData0(OFF);
+			setData1(ON);
+			setData2(ON);
+			setData3(OFF);
+			setData4(OFF);
+			setData5(OFF);
+			setData6(OFF);
+			setData7(OFF);
+		break;
+		case DISPLAY_CURSOR_ON:
+			setData0(ON);
+			setData1(ON);
+			setData2(ON);
+			setData3(ON);
+			setData4(OFF);
+			setData5(OFF);
+			setData6(OFF);
+			setData7(OFF);
+		break;
+		case DISPLAY_CURSOR_OFF:
+			setData0(OFF);
+			setData1(OFF);
+			setData2(ON);
+			setData3(ON);
+			setData4(OFF);
+			setData5(OFF);
+			setData6(OFF);
+			setData7(OFF);
 		break;
 	}
 	
@@ -286,4 +334,63 @@ void setCommandOnDataLine(lcdCommands commandToExecute) {
 
 void writeString(char* dataToWrite) {
 	
+	while (*dataToWrite) {
+		int asciiValue = (int) (*dataToWrite);
+		
+		printf("ASCII Char=%c Value=%i\n", (*dataToWrite), asciiValue);
+		
+		if ((0x1&asciiValue) == 0) {
+			setData0(OFF);
+		} else {
+			setData0(ON);
+		}
+		
+		if ((0x2&asciiValue) == 0) {
+			setData1(OFF);
+		} else {
+			setData1(ON);
+		}
+		
+		if ((0x4&asciiValue) == 0) {
+			setData2(OFF);
+		} else {
+			setData2(ON);
+		}
+		
+		if ((0x8&asciiValue) == 0) {
+			setData3(OFF);
+		} else {
+			setData3(ON);
+		}
+		
+		if ((0x10&asciiValue) == 0) {
+			setData4(OFF);
+		} else {
+			setData4(ON);
+		}
+		
+		if ((0x20&asciiValue) == 0) {
+			setData5(OFF);
+		} else {
+			setData5(ON);
+		}
+		
+		if ((0x40&asciiValue) == 0) {
+			setData6(OFF);
+		} else {
+			setData6(ON);
+		}
+		
+		if ((0x80&asciiValue) == 0) {
+			setData7(OFF);
+		} else {
+			setData7(ON);
+		}
+		
+		// Sets the Register Select to 1 so that the LCD expects a character.
+		setRegisterSelect(ON);
+		sendCommand();
+		
+		dataToWrite++;
+	}
 }
